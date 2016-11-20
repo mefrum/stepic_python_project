b3336c32acb6ff2967c0243de18390d57631d844
mefrum <mefrum@yandex.ru>
2016-11-20 22:18:58 +0300

    Change config nginx, gunicorn and init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo nginx reload
+cd /home/box/web/
+sudo gunicorn -b '0.0.0.0:8080' hello:app &
+cd /home/box/web/ask/
+sudo gunicorn -b '0.0.0.0:8000' ask.wsgi:application &
 sudo /etc/init.d/mysql start
+ 
+
 
