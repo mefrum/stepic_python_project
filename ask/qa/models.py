3eefdfd1cd51724d1b96274ce414072aa610f48a
mefrum <mefrum@yandex.ru>
2017-01-07 02:26:32 +0300

    add model manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,11 +7,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+       return Question.objects.order_by('added_at')[0:10]
+
+    def popular(self):
+        return Question.objects.order_by('rating')[0:10]
+
+
 class Question(models.Model):
+    objects = QuestionManager(  )
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField(default=1)
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='to_owner_q')
     likes = models.ManyToManyField(User)
 
@@ -23,9 +32,3 @@ class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
-class QuestionManager(models.Model):
-    def new(self):
-       return Question.objects.order_by('added_at')[0:10]
-
-    def popular(self):
-        return Question.objects.order_by('rating')[0:10]
